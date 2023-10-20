5e7d9bac3a884c4e6e565d5e92e5ab2b9572eb00
code-perceptkraft <code-perceptkraft>
2023-10-20 13:01:12 +0530

    Added Tool Header

--- app/src/main/java/com/example/photo_edit/EditActivity.kt
+++ app/src/main/java/com/example/photo_edit/EditActivity.kt
@@ -6,15 +6,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.example.photo_edit.ui.theme.PhotoeditTheme
+
+enum class TOOL(name: String) {
+    BRIGHTNESS("Brightness"),
+    SATURATION("Saturation")
+}
+
+data class ToolButton (
+    val tool: TOOL,
+    val onClick: () -> Unit,
+    val icon: Int = 0
+)
 
 class EditActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +63,40 @@ class EditActivity : ComponentActivity() {
 
                     val uri = Uri.parse(uriString)
 
-                    EditImage(uri = uri)
+                    val currentTool = remember {
+                        mutableStateOf<TOOL?>(null)
+                    }
+
+                    val brightness = remember {
+                        mutableStateOf(0f)
+                    }
+
+                    val saturation = remember {
+                        mutableStateOf(1f)
+                    }
+
+                    val tools = listOf(
+                        ToolButton(
+                            TOOL.BRIGHTNESS,
+                            {
+                                currentTool.value = TOOL.BRIGHTNESS
+                            },
+                            R.drawable.ic_bright
+                        ),
+                        ToolButton(
+                              TOOL.SATURATION,
+                            {
+                                currentTool.value = TOOL.SATURATION
+                            },
+                            R.drawable.ic_saturation
+                        )
+                    )
+
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        utilHeader(tools = tools, currentTOOL = currentTool)
+                        EditImage(uri = uri)
+                    }
+
                 }
             }
         }
@@ -50,3 +111,33 @@ fun EditImage(uri: Uri) {
         Image(painter = painter, contentDescription = null)
     }
 }
+
+@Composable
+fun utilHeader(tools: List<ToolButton>, currentTOOL: MutableState<TOOL?>) {
+    LazyRow(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)) {
+        items(tools) {
+           val icon = painterResource(id = it.icon)
+            IconButton(
+                onClick = it.onClick,
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(
+                        if (currentTOOL.value == it.tool)
+                            MaterialTheme.colorScheme.secondary
+                        else
+                            Color.White
+                    )
+                    .padding(4.dp)
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(painter = icon, contentDescription = null)
+                    Text(text = it.tool.name, fontSize = 12.sp)
+                }
+            }
+        }
+    }
+}
+
